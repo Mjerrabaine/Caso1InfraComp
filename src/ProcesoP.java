43be9c48b9998c13c05f6a9f4337cfe66f83a2fd
Mjerrabaine <85709451+Mjerrabaine@users.noreply.github.com>
2023-02-16 15:06:37 -0500

    Update ProcesoP.java

--- src/ProcesoP.java
+++ src/ProcesoP.java
@@ -1,4 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author usuario
+ */
 
 public abstract class ProcesoP extends Thread {
-	
+    private String nombre;
+    
+    public void cambiarNombre(String s){
+        this.nombre = s;
+    }
 }
