2b84436365837096ad5e17e4183eea5df1ac7224
Mjerrabaine <85709451+Mjerrabaine@users.noreply.github.com>
2023-02-16 15:06:57 -0500

    Update Producto.java

--- src/Producto.java
+++ src/Producto.java
@@ -1,11 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
 
+/**
+ *
+ * @author usuario
+ */
 public class Producto {
-	String cadena;
-	boolean color;
-	
-	
-	public Producto(String cadena, boolean color) {
-		this.cadena = cadena;
-		this.color = color;
-	}
+
+    private String cadena;
+    private boolean color;
+    private static int idProducto;
+    private String nombre;
+
+    public Producto(int idProducto, String cadena, boolean color) {
+        this.cadena = cadena;
+        this.color = color;
+        this.idProducto = idProducto;
+    }
+
+    public void cambiarNombre(String s) {
+        this.nombre = s;
+    }
 }
